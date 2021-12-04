931f1024d2cfdd0c4a146b43d6a5b9d68b7dec0b
RounakK <rounakkolyarex@gmail.com>
2021-12-04 22:50:13 +0530

    First Commit. Initial project setup

---
+++ src/main/java/com/log/LogTail/LogTailApplication.java
@@ -0,0 +1,13 @@
+package com.log.LogTail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogTailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogTailApplication.class, args);
+	}
+
+}
